d3c2e6176cf760dfd5cd4510ca731c0ef13faf9a
Darin Critchlow <darincritchlow@gmail.com>
2014-04-10 20:47:59 -0600

    HuffmanCoding.tex finished implemented code pull in

--- Huffman_Coding/huffman.py
+++ Huffman_Coding/huffman.py
@@ -1,3 +1,37 @@
+#!/usr/bin/python
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
+
+with open('Metamorphosis.bin', 'rb') as f:
+    data = f.read()
+
+def encode(symb2freq):
+    """Huffman encode the given dict mapping symbols to weights"""
+    heap = [[wt, [sym, ""]] for sym, wt in symb2freq.items()]
+    heapify(heap)
+    while len(heap) > 1:
+        lo = heappop(heap)
+        hi = heappop(heap)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+    # print sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+
+# txt = "this is an example for huffman encoding"
+symb2freq = defaultdict(int)
+for ch in data:
+    symb2freq[ch] += 1
+# print "Symbol\tWeight\tHuffman Code"
+huff = encode(symb2freq)
+# for p in huff:
+#     print "%s\t%s\t%s" % (p[0], symb2freq[p[0]], p[1])
+
+# with open("huffman_values.txt", 'wb') as f4:
+#     f4.write(str(huff))
+
 # meta = open('amt.png', 'rb')
 # print meta.mode
 # print meta.name
@@ -29,37 +63,3 @@
 
 # with open('Metamorphosis.bin', 'wb') as f3:
 #     f3.write(data)
-
-with open('Metamorphosis.bin', 'rb') as f:
-    data = f.read()
-
-
-from heapq import heappush, heappop, heapify
-from collections import defaultdict
-
-def encode(symb2freq):
-    """Huffman encode the given dict mapping symbols to weights"""
-    heap = [[wt, [sym, ""]] for sym, wt in symb2freq.items()]
-    heapify(heap)
-    while len(heap) > 1:
-        lo = heappop(heap)
-        hi = heappop(heap)
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]
-        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    # print sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
-    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
-
-# txt = "this is an example for huffman encoding"
-symb2freq = defaultdict(int)
-for ch in data:
-    symb2freq[ch] += 1
-# print "Symbol\tWeight\tHuffman Code"
-huff = encode(symb2freq)
-# for p in huff:
-#     print "%s\t%s\t%s" % (p[0], symb2freq[p[0]], p[1])
-
-# with open("huffman_values.txt", 'wb') as f4:
-#     f4.write(str(huff))
